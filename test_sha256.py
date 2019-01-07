d84e8d9973300c68d82c231e7100c216c8812c1f
alex9702 <alexsandromoreir@gmail.com>
2019-01-07 09:50:05 -0300

    testes

---
+++ test_sha256.py
@@ -0,0 +1,24 @@
+from unittest import  TestCase, main
+from hash.sha256 import Sha256
+
+frase = 'this is a test!'
+
+class ShaTests(TestCase):
+    
+
+    def test_sha_256_empty(self):
+        self.assertEqual(Sha256().hexdigest(), 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+        
+    def test_sha_256_frase(self):
+        self.assertEqual(Sha256(frase).hexdigest(), 'ca7f87917e4f5029f81ec74d6711f1c587dca0fe91ec82b87bb77aeb15e6566d')
+
+    def test_sha_256_frase_ten_times(self):
+        self.assertEqual(Sha256(frase*10).hexdigest(), '17c9617a9f78c2940179d58592a8109ff1f6c2173fd349ecc9060381b2e6a603')
+
+    def test_sha_256_update(self):
+        s = Sha256(frase)
+        s.update(frase)
+        self.assertEqual(s.hexdigest(), '4cb926cd6bbdc69567eaff4b34f2e8bd2fd2f3d393fe8589b99a33c68dfecf5a')
+
+if __name__ == "__main__":
+    main()
---
+++ test_sha512.py
@@ -0,0 +1,25 @@
+from unittest import  TestCase, main
+from hash.sha512 import Sha512
+
+frase = 'this is a test!'
+
+class ShaTests(TestCase):
+    
+
+    def test_sha_512_empty(self):
+        self.assertEqual(Sha512().hexdigest(),'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e')
+        
+    def test_sha_512_frase(self):
+        self.assertEqual(Sha512(frase).hexdigest(), '5746df6112981b3236c15055113e14809578b42d9307f3dd21ef628cb2c78d6ca3f191c2402d94f98892729c41b4e3f97620a893cd0481a2846c9c166ed0e6de')
+
+    def test_sha_512_frase_ten_times(self):
+        self.assertEqual(Sha512(frase*10).hexdigest(), '83d57e91db26823110d7c3b387f457b71548458809227d60cf298ab19da67c6cb91f9ed34ead4b659174ac3f753ae8e99a5ebbfa0c48659fdf2445e16dadf17d')
+
+    def test_sha_512_update(self):
+        s = Sha512(frase)
+        s.update(frase)
+
+        self.assertEqual(s.hexdigest(), '7693c3358b77f120c99b964578d4f338bc29f209cc78a3ac43727e72eb7f1472ddeddfafd8288fab50d82af5d44bfbb38ccbc960d37a7fef0a719e7bda14a1ee')
+
+if __name__ == "__main__":
+    main()
